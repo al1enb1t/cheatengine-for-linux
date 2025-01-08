4ab5d72e234480938705632600ebefd60b366751
robert <myemail@proton.me>
2025-01-08 11:59:28 +0000

    Fix ISS-1253983

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 
 # This is a simple wrapper script to make compiling a bit easier.
 # Configure is too verbose and curious about many things and not
 # sufficiently eloquent about others such as available options.
 
-# Bail out on unexpected errors
-set -e
 
 # This is not interactive
 exec </dev/null
